6bc3c1a2c818537abd3ad27f502d181f9e9ce7a1
Andrej <andrej.hafn3r@gmail.com>
2018-06-12 14:25:25 +0200

    Added  Android project

---
+++ ScanItAndroid/app/src/main/cpp/ImageProcessing.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Likewise on 12. 06. 2018.
+//
+
+#include "ImageProcessing.h"
---
+++ ScanItAndroid/app/src/main/cpp/Line.h
@@ -0,0 +1,8 @@
+//
+// Created by Likewise on 12. 06. 2018.
+//
+
+#ifndef SCANITANDROID_LINE_H
+#define SCANITANDROID_LINE_H
+
+#endif //SCANITANDROID_LINE_H
